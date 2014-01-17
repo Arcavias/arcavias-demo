1fdf774ec6185843c7643f55a7e0d2aad8622ed3
Norbert <n.sendetzky@metaways.de>
2014-01-17 16:27:35 +0100

    Enables full page caching by the web browsers for one minute

--- zfapp/library/Application/Controller/Action/Abstract.php
+++ zfapp/library/Application/Controller/Action/Abstract.php
@@ -165,6 +165,12 @@ abstract class Application_Controller_Action_Abstract extends Zend_Controller_Ac
 		}
 
 		$this->view->params = $params;
+
+
+		$this->getResponse()
+			->setHeader( 'Expires', date( 'r', time() + 60 ), true )
+			->setHeader( 'Cache-Control', 'max-age=60', true )
+			->setHeader( 'Pragma', '', true );
 	}
 
 
