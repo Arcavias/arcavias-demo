4bc1a4799142e4730dcb78d9afbd0ea27195ae4a
Norbert <n.sendetzky@metaways.de>
2014-02-05 14:27:28 +0100

    Replaces meta tag for content type in HTML head by HTTP header

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,7 @@
  * @license LGPLv3, http://www.arcavias.com/en/license
 */
 
+header( 'Content-type: text/html; charset=UTF-8' );
 
 try
 {
@@ -33,7 +34,6 @@ try
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Arcavias ExtJS Admin Interface</title>
 	<script type="text/javascript">
 
