40942cc24560d6bce4780b607d120068d56a7e36
Norbert <n.sendetzky@metaways.de>
2014-02-21 16:17:32 +0100

    Uses configured catalog filter instead of only the search part

--- zfapp/application/controllers/AccountController.php
+++ zfapp/application/controllers/AccountController.php
@@ -16,25 +16,36 @@ class AccountController extends Application_Controller_Action_Abstract
 	public function historyAction()
 	{
 		$startaction = microtime( true );
-		$context = Zend_Registry::get( 'ctx' );
 
 		try
 		{
 			$arcavias = $this->_getArcavias();
+			$context = Zend_Registry::get( 'ctx' );
 			$templatePaths = $arcavias->getCustomPaths( 'client/html' );
 
-			$this->view->account = Client_Html_Account_History_Factory::createClient( $context, $templatePaths );
-			$this->view->account->setView( $this->_createView() );
-			$this->view->account->process();
 
-			$filter = Client_Html_Catalog_Filter_Factory::createClient( $context, $templatePaths );
-			$this->view->searchfilter = $filter->getSubClient( 'search' );
+			$conf = array( 'client' => array( 'html' => array(
+				'catalog' => array( 'filter' => array(
+					'default' => array( 'subparts' => array( 'search' ) )
+				) )
+			) ) );
+
+			$localContext = clone $context;
+			$localConfig = new MW_Config_Decorator_Memory( $localContext->getConfig(), $conf );
+			$localContext->setConfig( $localConfig );
+
+			$this->view->searchfilter = Client_Html_Catalog_Filter_Factory::createClient( $localContext, $templatePaths );
 			$this->view->searchfilter->setView( $this->_createView() );
-			$this->view->searchfilter->process();
+
 
 			$this->view->minibasket = Client_Html_Basket_Mini_Factory::createClient( $context, $templatePaths );
 			$this->view->minibasket->setView( $this->_createView() );
 			$this->view->minibasket->process();
+
+
+			$this->view->account = Client_Html_Account_History_Factory::createClient( $context, $templatePaths );
+			$this->view->account->setView( $this->_createView() );
+			$this->view->account->process();
 
 			$this->render( 'index' );
 		}
