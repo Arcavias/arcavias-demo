1c732066acdf4dfade9e2882b277484a8bee1cda
Norbert <n.sendetzky@metaways.de>
2014-02-22 11:10:44 +0100

    Re-adds mini basket to enable updating it on basket changes

--- zfapp/application/controllers/BasketController.php
+++ zfapp/application/controllers/BasketController.php
@@ -17,32 +17,34 @@ class BasketController extends Application_Controller_Action_Abstract
 	{
 		$startaction = microtime( true );
 
-
-		$arcavias = $this->_getArcavias();
-		$context = Zend_Registry::get( 'ctx' );
-		$templatePaths = $arcavias->getCustomPaths( 'client/html' );
-
-		$conf = array( 'client' => array( 'html' => array(
-			'catalog' => array( 'filter' => array(
-				'default' => array( 'subparts' => array( 'search' ) )
-			) )
-		) ) );
-
-		$localContext = clone $context;
-		$localConfig = new MW_Config_Decorator_Memory( $localContext->getConfig(), $conf );
-		$localContext->setConfig( $localConfig );
-
-		$this->view->searchfilter = Client_Html_Catalog_Filter_Factory::createClient( $localContext, $templatePaths );
-		$this->view->searchfilter->setView( $this->_createView() );
-
 		try
 		{
 			$arcavias = $this->_getArcavias();
+			$context = Zend_Registry::get( 'ctx' );
 			$templatePaths = $arcavias->getCustomPaths( 'client/html' );
+
+			$conf = array( 'client' => array( 'html' => array(
+				'catalog' => array( 'filter' => array(
+					'default' => array( 'subparts' => array( 'search' ) )
+				) )
+			) ) );
+
+			$localContext = clone $context;
+			$localConfig = new MW_Config_Decorator_Memory( $localContext->getConfig(), $conf );
+			$localContext->setConfig( $localConfig );
+
+			$this->view->searchfilter = Client_Html_Catalog_Filter_Factory::createClient( $localContext, $templatePaths );
+			$this->view->searchfilter->setView( $this->_createView() );
+
 
 			$this->view->basket = Client_Html_Basket_Standard_Factory::createClient( $context, $templatePaths );
 			$this->view->basket->setView( $this->_createView() );
 			$this->view->basket->process();
+
+
+			$this->view->minibasket = Client_Html_Basket_Mini_Factory::createClient( $context, $templatePaths );
+			$this->view->minibasket->setView( $this->_createView() );
+			$this->view->minibasket->process();
 
 			$this->render( 'index' );
 		}
